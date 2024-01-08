4cb297ea21f6eecb258437953bb5f578f68053b8
Edgar Quiroz <edgar_qp11@outlook.com>
2024-01-08 13:49:41 -0600

    Agrega hola mundo rama team

--- src/main/java/com/example/msprueba/Application.java
+++ src/main/java/com/example/msprueba/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
   public static void main(String[] args) {
+    System.out.println("Hola mundo rama team");
     SpringApplication.run(Application.class, args);
   }
 }
